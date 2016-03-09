71051b2eb93ce5c231687a77a6a7b6e8ea9fbaa8
Renan Diniz <renan.almeida@embedded.ufcg.edu.br>
2016-03-09 10:24:14 -0300

    adding properties

--- Gstreamer-VoIP/audiostreamer/src/main/java/org/github/audiostreamer/AudioManager.java
+++ Gstreamer-VoIP/audiostreamer/src/main/java/org/github/audiostreamer/AudioManager.java
@@ -3,6 +3,9 @@ package org.github.audiostreamer;
 import android.content.Context;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by renan on 02/03/16.
  */
@@ -10,15 +13,24 @@ public class AudioManager {
 
     public static int SPEEX = 97;
     public static int PCMA = 8;
+    final private int DEFAULT_SAMPLE_RATE = 16000;
 
+    // Properties
+    /**
+     * Speex encoding mode.
+     */
+    public static String MODE = "mode";
+    /**
+     * Sampling rate.
+     */
+    public static String RATE = "rate";
 
-    private String TAG = "AudioManager";
     private Context mContext;
     private AudioStreamer audioSender;
     private AudioStreamer audioReceiver;
     private String senderPipeline;
     private String receiverPipeline;
-    private int SampleRate = 16000;
+    private String TAG = "AudioManager";
 
     public AudioManager(Context context) {
         this.mContext = context;
@@ -26,27 +38,52 @@ public class AudioManager {
         audioReceiver = new AudioStreamer(mContext, "audioReceiver");
     }
 
-    public void startVOIPStreaming(int remoteRtpPort, String remoteIp, int localPort, int codecPayloadType) {
-        Log.d(TAG, "startVOIPStreaming() called with: " + "remoteRtpPort = [" + remoteRtpPort + "], remoteIp = [" + remoteIp + "], localPort = [" + localPort + "], codec = [" + codecPayloadType + "]");;
+    public void startVOIPStreaming(int remoteRtpPort, String remoteIp, int localPort, int codecPayloadType, Map<String, String> properties) {
+        Log.d(TAG, "startVOIPStreaming() called with: " + "remoteRtpPort = [" + remoteRtpPort + "], remoteIp = [" + remoteIp + "], localPort = [" + localPort + "], codec = [" + codecPayloadType + "]");
 
         if (audioSender != null) audioSender.stop();
         if (audioReceiver != null) audioReceiver.stop();
 
-        String audioSenderCaps = "audio/x-raw, channels=1, rate=" + SampleRate;
+        String audioSenderCaps = "audio/x-raw, channels=1, rate=" + getSampleRate(properties, DEFAULT_SAMPLE_RATE);
 
-        if (codecPayloadType == 97) {
-            String audioReceiverCaps = " caps=\"application/x-rtp, media=(string)audio,clock-rate=(int)" + SampleRate + ",encoding-name=(string)SPEEX,encoding-params=(string)1,octet-align=(string)1\"";
-            senderPipeline = "openslessrc ! audioconvert noise-shaping=medium ! audioresample !" + audioSenderCaps + " ! speexenc ! rtpspeexpay pt=97 ! udpsink host=" + remoteIp + " port=" + remoteRtpPort;
+        if (codecPayloadType == SPEEX) {
+            String audioReceiverCaps = " caps=\"application/x-rtp, media=(string)audio,clock-rate=(int)" + getSampleRate(properties, DEFAULT_SAMPLE_RATE) + ",encoding-name=(string)SPEEX,encoding-params=(string)1,octet-align=(string)1\"";
+
+            String speexEnc = "speexenc " + getPropertyString(AudioManager.MODE, properties, "");
+
+            senderPipeline = "openslessrc ! audioconvert noise-shaping=medium ! audioresample !" + audioSenderCaps + " ! " + speexEnc + " ! rtpspeexpay pt=97 ! udpsink host=" + remoteIp + " port=" + remoteRtpPort;
             audioSender.startVOIPStreaming(senderPipeline);
             receiverPipeline = "udpsrc port=" + localPort + audioReceiverCaps + " ! rtpspeexdepay ! speexdec ! audioconvert ! audioresample ! openslessink name=openslessink stream-type=voice";
             audioReceiver.startVOIPStreaming(receiverPipeline);
 
-        } else if (codecPayloadType == 8) {
-            String audioReceiverCaps = " caps=\"application/x-rtp, media=(string)audio,clock-rate=(int)" + SampleRate + ",encoding-name=(string)PCMA\" ";
+        } else if (codecPayloadType == PCMA) {
+            String audioReceiverCaps = " caps=\"application/x-rtp, media=(string)audio,clock-rate=(int)" + getSampleRate(properties, DEFAULT_SAMPLE_RATE) + ",encoding-name=(string)PCMA\" ";
             senderPipeline = "openslessrc ! audioconvert noise-shaping=medium ! audioresample ! " + audioSenderCaps + " ! alawenc ! rtppcmapay pt=8 ! udpsink host=" + remoteIp + " port=" + remoteRtpPort;
             audioSender.startVOIPStreaming(senderPipeline);
             receiverPipeline = "udpsrc port=" + localPort + audioReceiverCaps + " ! rtppcmadepay ! alawdec ! audioconvert ! audioresample ! openslessink name=openslessink stream-type=voice";
             audioReceiver.startVOIPStreaming(receiverPipeline);
+        }
+    }
+
+    public void startVOIPStreaming(int remoteRtpPort, String remoteIp, int localPort, int codecPayloadType) {
+        startVOIPStreaming(remoteRtpPort, remoteIp, localPort, codecPayloadType, new HashMap<String, String>());
+    }
+
+    public String getPropertyString(String property, Map<String, String> properties, String defaultValue) {
+        if (properties.get(property) != null) {
+            return property + "=" + properties.get(property);
+        } else if (defaultValue != null) {
+            return property + "=" + defaultValue;
+        } else {
+            return "";
+        }
+    }
+
+    public int getSampleRate(Map<String, String> properties, int defaultValue) {
+        if (properties.get(RATE) != null) {
+            return new Integer(properties.get(RATE));
+        } else {
+            return defaultValue;
         }
     }
 
@@ -59,11 +96,10 @@ public class AudioManager {
         }
     }
 
-    public void release(){
+    public void release() {
         if (audioSender != null) {
             audioSender.finalize();
-        }
-        else if (audioReceiver != null) {
+        } else if (audioReceiver != null) {
             audioReceiver.finalize();
         }
     }
@@ -90,14 +126,4 @@ public class AudioManager {
             audioSender.resume();
         }
     }
-
-    public int getSampleRate() {
-        return SampleRate;
-    }
-
-    public void setSampleRate(int sampleRate) {
-        SampleRate = sampleRate;
-    }
-
-
 }
